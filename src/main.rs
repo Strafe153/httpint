9ba3763a4afc7111c3175674c955c6a818622abf
Vitalii <knish.vital@gmail.com>
2026-05-27 01:02:42 +0300

    feat: add basic headers UI logic

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,49 @@ include_modules!();
 fn main() -> Result<(), PlatformError> {
     let window = AppWindow::new()?;
 
-    set_initial_headers(&window);
+    let headers_model = Rc::new(VecModel::from(vec![Header {
+        name: SharedString::from("Accept"),
+        value: SharedString::from("*/*"),
+    }]));
+
+    window.set_headers(headers_model.clone().into());
 
     window.on_send(|url, t| {
         println!("{}: {}", t, url);
     });
 
+    let model_clone = headers_model.clone();
+    window.on_remove(move |index: i32| {
+        model_clone.remove(index as usize);
+    });
+
+    let model_clone = headers_model.clone();
+    window.on_edit_name(move |i, value| {
+        let i = i as usize;
+
+        if let Some(mut header) = model_clone.row_data(i) {
+            header.name = value;
+            model_clone.set_row_data(i, header);
+        }
+    });
+
+    let model_clone = headers_model.clone();
+    window.on_edit_value(move |i, value| {
+        let i = i as usize;
+
+        if let Some(mut header) = model_clone.row_data(i) {
+            header.value = value;
+            model_clone.set_row_data(i, header);
+        }
+    });
+
+    let model_clone = headers_model.clone();
+    window.on_add(move || {
+        model_clone.push(Header {
+            name: SharedString::from(""),
+            value: SharedString::from(""),
+        });
+    });
+
     window.run()
-}
-
-fn set_initial_headers(window: &AppWindow) {
-    let initial_headers = vec![Header {
-        name: SharedString::from("Accept"),
-        value: SharedString::from("*/*"),
-    }];
-
-    let mut headers: Vec<Header> = window.get_headers().iter().collect();
-    headers.extend(initial_headers);
-
-    let headers_model = Rc::new(VecModel::from(headers));
-    window.set_headers(headers_model.into());
 }
